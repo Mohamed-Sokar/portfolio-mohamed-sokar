512e3fe7e4d4bd55507fa753fc11eaa865a3a84d
marsokar <aboomarsokar@gmail.com>
2025-05-26 18:55:01 +0300

    adding shadow to header when scroll in dark mode

--- components/app-header.vue
+++ components/app-header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="sticky top-0 z-50 w-full border-b border-gray-200 dark:border-gray-600 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+    class="sticky top-0 z-50 w-full border-b border-gray-200 dark:border-gray-600 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-gray-600"
   >
     <UICustomContainer class="flex h-16 items-center justify-between">
       <NuxtLink
